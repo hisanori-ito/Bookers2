10be47981f3edc2eaa265ec6451d5e489d1ca0bc
EC2 Default User <ec2-user@ip-172-31-4-12.ec2.internal>
2022-01-17 15:56:41 +0000

    [Add]モデルの紐付け、ルーティング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   #def after_sign_in_path_for(resource)
-    # ユーザーの詳細画面マイページへ設定する
+    # ユーザーの詳細画面マイページへ設定する。pathの記入
   #end
 
   #def after_sign_out_path_for(resource)
