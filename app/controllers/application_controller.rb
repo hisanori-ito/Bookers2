93becda58bc49553ef814d0c80d93285c1fc048c
EC2 Default User <ec2-user@ip-172-31-4-12.ec2.internal>
2022-01-17 14:53:33 +0000

    [Add]deviseでの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #def after_sign_in_path_for(resource)
+    # ユーザーの詳細画面マイページへ設定する
+  #end
+
+  #def after_sign_out_path_for(resource)
+    # 元々ルートだから要らない。あとで消す
+  #end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
